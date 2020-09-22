41cc05c6a1b7874263cc338f9d43336dd4fe452b
Jordan Roberts <jdrobs19@gmail.com>
2020-09-21 19:32:05 -0600

    refactor js code

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-// var commandLineArgs = process.argv
-// console.log(commandLineArgs)
+const fs = require('fs');
+const generatePage = require('./src/page-template.js')
 
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = (profileDataArgs) => {
-    for (let i = 0; i < profileDataArgs.length; i += 1) {
-        console.log(profileDataArgs[i]);
-    }
-console.log("===========");
+const [name, github] = profileDataArgs;
 
-profileDataArgs.forEach(profileItem =>
-    console.log(profileItem));
-};
+fs.writeFile('index.html', generatePage(name, github), err =>{
+    if(err) throw err;
+    console.log('Porfolio complete! Checkout index.html to see the output!');
+});
+
+// var commandLineArgs = process.argv
+// console.log(commandLineArgs)
 
 
 
-printProfileData(profileDataArgs);
+// const printProfileData = (profileDataArgs) => {
+//     for (let i = 0; i < profileDataArgs.length; i += 1) {
+//         console.log(profileDataArgs[i]);
+//     }
+// console.log("===========");
+
+// profileDataArgs.forEach(profileItem =>
+//     console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
+
