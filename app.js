73b8ee7602791d0d7f38deafaf19b8851e09b70c
Jordan Roberts <jdrobs19@gmail.com>
2020-09-22 19:20:03 -0600

    added inquirer

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js')
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+])
+.then(answers => console.log(answers));
 
-const [name, github] = profileDataArgs;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js')
 
-fs.writeFile('index.html', generatePage(name, github), err =>{
-    if(err) throw err;
-    console.log('Porfolio complete! Checkout index.html to see the output!');
-});
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+// const [name, github] = profileDataArgs;
+
+// fs.writeFile('index.html', generatePage(name, github), err =>{
+//     if(err) throw err;
+//     console.log('Porfolio complete! Checkout index.html to see the output!');
+// });
 
 // var commandLineArgs = process.argv
 // console.log(commandLineArgs)
